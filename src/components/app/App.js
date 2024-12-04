0f2bfa301c91446d295816313f684ebd6a6519ba
Vinor <vinorval@yandex.ru>
2024-12-04 10:12:09 +0300

    add a background image in the header of the website

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,11 @@
+import { Routes, Route } from 'react-router';
+
+import Header from "../header/Header";
+
 function App() {
   return (
     <div className="app">
+      <Header />
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,11 +5,11 @@ import Navigation from "../navigation/Navigation";
 
 const Header = () => {
     const location = useLocation();
-    //в переменую записываем название страницы, чтоб правильно выбрать фотографию для фона шапки сайта
+    //в переменую записываем название страницы, чтоб правильно выбрать фотографию для фона промо сайта
     let path = (location.pathname === '/') ? 'home' : location.pathname.split("/")[2];
 
     return (
-        <header className={`header header_background_${path} ${ location.pathname === "/" ? 'header_height_big' : 'header_height_small'}`}>
+        <header className={`header header__background header_page_${path} ${ location.pathname === "/" ? 'header_height_big' : 'header_height_small'}`}>
             <Navigation />
         </header>
     );
